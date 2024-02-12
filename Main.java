85f45aae6726781a18affd69a937e8137d73f05f
lamia49 <80590112+lamia49@users.noreply.github.com>
2024-02-13 01:58:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Q1
+        Scanner input = new Scanner(System.in);
+      /*  System.out.println("Enter your Visit type");
+        String user = input.nextLine();
+        if (user.equalsIgnoreCase("admin")) {
+            System.out.println("Welcom admin");
+        } else if (user.equalsIgnoreCase("suprviser")) {
+            System.out.println("Welcom Suprviser");
+        } else if (user.equalsIgnoreCase("user")) {
+            System.out.println("Welcom user");
+
+        } else {
+            System.out.println("not  authorize");
+        }*/
+
+        //Q2
+
+   /*     System.out.print("Enter number1 :");
+        int num1 = input.nextInt();
+        System.out.print("Enter number2 :");
+        int num2 = input.nextInt();;
+        System.out.print("Enter number3 :");
+        int num3= input.nextInt();
+        if(num1>num2&&num1>num2) {
+            System.out.println("The greatest number is: "+num1);
+        } else if (num2>num1&&num2>num3) {
+            System.out.println("The greatest number is: "+num2);
+        } else if (num3>num1&&num3>num2) {
+            System.out.println("The greatest number is: "+num3);
+        }*/
+
+        //Q3
+
+    /*    System.out.println("Enter number from 1 to 7: ");
+        int num= input.nextInt();
+        switch (num){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3 :
+                System.out.println("Tusday");
+                break;
+            case 4:
+                System.out.println("Wensday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case  7:
+                System.out.println("Satrday");
+                break;
+            default:
+                System.out.println("Wrong number");
+                break;
+        }*/
+
+        //Q4
+
+    /*    System.out.print("Enter the Score: ");
+        int Score = input.nextInt();
+        if(Score>=100) {
+                System.out.print("Lettar Grade A");
+            } else if (Score <= 89) {
+                System.out.print("Lettar Grade B");
+            } else if (Score <= 70) {
+                System.out.print("Lettar Grade C");
+            } else if (Score <= 60) {
+                System.out.print("Lettar Grade D");
+            } else if (Score <= 59) {
+                System.out.print("Failed");
+            }*/
+
+        //Q5
+
+  /*      System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        if (age < 13) {
+            System.out.println("Child");
+        } else if (age >= 13 && age <19) {
+            System.out.println("Teenger");
+        } else if (age>20) {
+            System.out.println("Adult");
+        }*/
+
+    }
+}
+
+
+
+
+
